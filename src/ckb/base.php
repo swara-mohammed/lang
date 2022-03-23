eba2b55663fd46826d5613908b18181b8b58ef97
Swara <swara09@gmail.com>
2022-03-23 22:15:03 +0100

    Create base.php

---
+++ src/ckb/base.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Base translations are words or word combinations that can occur frequently
+    | and do not specifically belong to any part of the litstack application.
+    |
+    */
+
+    'no_item_found'    => 'هیچ :item نەدۆزرایەوە',
+    'no_item_selected' => 'هیچ :item هەڵنەبژێردراوە',
+    'item_delete'      => 'سڕینەوەی :item',
+    'item_deleted'     => ':item سڕایەوە',
+    'item_enter'       => 'زیادکردنی :item',
+    'item_create'      => 'دروستکردنی :item',
+    'item_remove'      => 'سڕینەوەی :item',
+    'item_select'      => 'هەڵبژاردنی :item',
+    'item_order'       => 'ڕیزکردنی :item',
+    'item_ordered'     => ':item ڕیزکرا',
+    'item_search'      => 'گەڕانی :item',
+    'item_add'         => 'زیادکردنی :item',
+    'item_added'       => ':item زیادکرا',
+    'item_assign'      => 'پێسپاردنی :item',
+    'item_settings'    => ':item ڕێکخستنەکان',
+    'item_item'        => ':item بڕگە',
+    'user_hello'       => 'سڵاو :user',
+    'profile'          => 'زانیاری تاکەکەسیی',
+    'settings'         => 'ڕێکخستنەکان',
+    'password'         => 'تێپەڕەوشە',
+    'drag_and_drop'    => 'ڕێکشان و دانان',
+    'name'             => 'ناو',
+    'user'             => 'بەکارهێنەر',
+    'users'            => 'بەکارهێنەران',
+    'email'            => 'ئیمەیڵ',
+    'email_address'    => 'ناونیشانی ئیمەیڵ',
+    'language'         => 'زمان',
+    'system_language'  => 'زمانی سیستەم',
+    'general'          => 'گشتی',
+    'security'         => 'پاراستن',
+    'device'           => 'ئامێر',
+    'logout'           => 'چوونەدرەوە',
+    'change_order'     => 'گۆڕینی ڕیز',
+    'login'            => 'چوونەژوورەوە',
+    'close'            => 'داخستن',
+    'save'             => 'پاشەکەوتکردن',
+    'saved'            => 'پاشەکەوتکرا',
+    'done'             => 'تەواوبوو',
+    'undo_changes'     => 'گەڕاندنەوەی گۆڕانکارییەکان',
+    'delete'           => 'سڕینەوە',
+    'image'            => 'وێنە',
+    'crop'             => 'بڕین',
+    'search'           => 'گەڕان',
+    'first_name'       => 'ناوی یەکەم',
+    'last_name'        => 'ناوی دووەم',
+    'username'         => 'ناوی بەکارهێنەر',
+    'cancel'           => 'پەشیمانبوونەوە',
+    'location'         => 'شوێن',
+    'action'           => 'کردار|کردارەکان',
+    'role'             => 'ڕۆڵ',
+    'roles'            => 'ڕۆڵەکان',
+    'read'             => 'خوێندنەوە',
+    'update'           => 'دەستکاریکردن',
+    'sort'             => 'جۆرکردن',
+    'create'           => 'دروستکردن',
+    'delete'           => 'سڕینەوە',
+    'permissions'      => 'ڕێگەپێدانەکان',
+    'group'            => 'گرووپ',
+    'unauthorized'     => 'ئەم کردارە ڕێگەپێنەدراوە!',
+    'page'             => 'لاپەڕە',
+    'all'              => 'هەموو',
+    'pages'            => 'لاپەڕەکان',
+    'hello'            => 'سڵاو',
+    'filter'           => 'پاڵاوتن',
+    'toggle_all'       => 'گۆڕینی هەموو',
+    'messages'         => [
+        'order_changed' => 'ڕیزکردن بەسەرکەوتوویی گۆڕا',
+        'are_you_sure'  => 'ئایا دڵنیایت؟ ئەم کردارە ناتوانرێت بگەڕێندرێتەوە.',
+    ],
+];
